9e14a037fac36417ac73b8cb99c275f098b5d51a
YaroslavS23 <senkivy@list.ru>
2023-08-12 17:21:53 +0300

    com1

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,86 @@
 #include <Arduino.h>
 #include <WiFi.h>
-const char* ssid     = "MERCUSYS_B85E";
-const char* password = "47950450";
 
 #include <EmonLib.h>             
 EnergyMonitor emon1;
 
+#include <GyverPortal.h>
+#include <EEPROM.h>
+
+struct LoginPass {
+  char ssid[20];
+  char pass[20];
+};
+
+LoginPass lp;
+
+void build() {
+  GP.BUILD_BEGIN();
+  GP.THEME(GP_DARK);
+
+  GP.FORM_BEGIN("/login");
+  GP.TEXT("lg", "Login", lp.ssid);
+  GP.BREAK();
+  GP.TEXT("ps", "Password", lp.pass);
+  GP.SUBMIT("Submit");
+  GP.FORM_END();
+
+  GP.BUILD_END();
+}
+
+void action(GyverPortal& p) {
+  if (p.form("/login")) {      // кнопка нажата
+    p.copyStr("lg", lp.ssid);  // копируем себе
+    p.copyStr("ps", lp.pass);
+    EEPROM.put(0, lp);              // сохраняем
+    EEPROM.commit();                // записываем
+    WiFi.softAPdisconnect();        // отключаем AP
+  }
+}
+
+void loginPortal() {
+  Serial.println("Portal start");
+
+  // запускаем точку доступа
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("WiFi Config");
+
+  // запускаем портал
+  GyverPortal ui;
+  ui.attachBuild(build);
+  ui.start();
+  ui.attach(action);
+
+  // работа портала
+  while (ui.tick());
+}
+
 void setup() {
-    Serial.begin(115200);
-    delay(10);
-    
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
- 
-    delay(500);
+  Serial.begin(115200);
 
   emon1.voltage(34, 196.50, 1.7);  // Voltage: input pin, calibration, phase_shift
   emon1.current(36, 1.1);       // Current: input pin, calibration.
+
+  EEPROM.begin(100);
+  EEPROM.get(0, lp);
+
+  // если кнопка нажата - открываем портал
+  pinMode(2, INPUT_PULLUP);
+  if (!digitalRead(2)) loginPortal();
+
+  // пытаемся подключиться
+  Serial.print("Connect to: ");
+  Serial.println(lp.ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(lp.ssid, lp.pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected! Local IP: ");
+
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
